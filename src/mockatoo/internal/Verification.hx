66695d62a0764537dcc4fb1abfdee981210c754a
laurent.deketelaere <laurent.deketelaere@doclerholding.com>
2019-10-21 00:52:03 +0200

    implements Dynamic<Dynamic> not supported anymore

--- src/mockatoo/internal/Verification.hx
+++ src/mockatoo/internal/Verification.hx
@@ -6,12 +6,9 @@ import mockatoo.Mockatoo;
 	Dynamic class that is created for verification.
 	Each dynamic method calls to a specific MockMethod.
 */
-class Verification implements Dynamic<Dynamic>
-{
-	public var mode:VerificationMode;
+@:forward() abstract Verification(Dynamic) from Dynamic to Dynamic {
 
-	public function new(mode:VerificationMode)
-	{
-		this.mode = mode;	
-	}
+	public function new (mode:VerificationMode) {
+        this = {mode: mode};
+    }
 }
